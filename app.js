cec4d860422e20d59b7b3409aae5e2ce37b46bb3
YosKad <yoskadtl@gmail.com>
2023-01-31 11:24:56 +0200

    create contact section and make the website resposive to mobile

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var tablinks = document.getElementsByClassName("tab-links")
 var tabcontents = document.getElementsByClassName("tab-contents")
+var sidemenu = document.getElementById("sidemenu")
 
 function opentab(tabname) {
     for(tablink of tablinks) {
@@ -12,3 +13,13 @@ function opentab(tabname) {
     document.getElementById(tabname).classList.add("active-tab")
 
 }
+
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
+
+
+
