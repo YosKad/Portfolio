a6ec7031245ee834237098507a6bf7832ee68523
YosKad <yoskadtl@gmail.com>
2023-02-12 11:14:15 +0200

    add message when dliver succsue

--- form.js
+++ form.js
@@ -1,10 +1,17 @@
 const scriptURL =
   "https://script.google.com/macros/s/AKfycbwt2QHx7LnQ6SR5tlnxq-WeO9TCkoWphh-2bJbUwe2YMyw8p0rTSnejQWYEQOgMsx_T/exec";
 const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById('msg')
+
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   fetch(scriptURL, { method: "POST", body: new FormData(form) })
-    .then((response) => console.log("Success!", response))
+    .then((response) => {msg.innerHTML = ' <i class="fa-regular fa-paper-plane"></i> Message Sent Successfully'
+  setTimeout(() => {
+    msg.innerHTML = ""
+  }, 5000);
+  form.reset()
+  })
     .catch((error) => console.error("Error!", error.message));
 });
